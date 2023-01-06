50c415c1cb5d220357e47b41f7f5696e50cef310
JasonLiaoZeng <jlz857609223@yahoo.com>
2023-01-05 20:58:28 -0800

    finished session1 lecture

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+    ){
+  }
+
+  jumpToSession1(){
+    this.router.navigate(['session1-page']);
+  }
 }
